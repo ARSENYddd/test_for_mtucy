90a221c4c7a06ab90d7b51ba905e723a54d92743
Ar$en <76821344+ARSENYddd@users.noreply.github.com>
2024-05-23 19:25:22 +0300

    sfsf

---
+++ yyyyy.py
@@ -0,0 +1,7 @@
+import csv
+with open("sales_data.csv", mode="w", encoding='utf-8') as w_file:
+    file_writer = csv.writer(w_file, delimiter = ",", lineterminator="\r")
+    file_writer.writerow(["Имя", "Класс", "Возраст"])
+    file_writer.writerow(["Женя", "3", "10"])
+    file_writer.writerow(["Саша", "5", "12"])
+    file_writer.writerow(["Маша", "11", "18"])
